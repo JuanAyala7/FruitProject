2562b1bf14eaaa0bb4241437156ef91fa3c4a49d
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-12-23 12:26:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
 //jshint esversion:6
 
-const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const { MongoClient } = require("mongodb");
 
-// Connection URL
-const url = 'mongodb://0.0.0.0:27017/';
+// Connection URL (Replace the url string with your connection string.)
+// const url = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+const url = "mongodb://0.0.0.0:27017";
+
+// Create a new MongoClient
 const client = new MongoClient(url);
 
-// Database Name
-const dbName = 'myProject';
-
 async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
+  try {
+    // ----- Use connect method to connect to the server -----
+    await client.connect();
+    console.log('Connected successfully to server');
 
-  // the following code examples can be pasted here...
+    // ----- Database Name -----
+    const database = client.db('fruitsDB');
+    // ----- Create a collection -----
+    const fruitsCollection = database.collection('fruits');
 
-  return 'done.';
+    // ----- Insert a document (data in row) -----
+    // Create an array of documents to insert
+    const docs = [
+        {
+            name: "Apple",
+            score: 8,
+            review: "Great Fruit"},
+        {
+            name: "Orange",
+            score: 6,
+            review: "Kinda Sour"},
+        {
+            name: "Banana",
+            score: 9,
+            review: "Great Stuff"}
+       ];
+
+    const insertResult = await fruitsCollection.insertMany(docs);
+    console.log("Inserted " + insertResult.insertedCount + " documents into the fruitsCollection.");
+
+    // ----- 3rd cmd (to check whether the data is in mongosh) -----
+    // (1)mongosh
+    // (2)show dbs
+    // (3)use <databseName>
+    // (4)show collections
+    // (5)db.<collectionName>.find()
+
+    // ----- Find all documents -----
+    const findResult = await fruitsCollection.find({}).toArray();
+    console.table(findResult);
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+main().catch(console.dir);
